71383c6bbc9939fadea01b3fbb78bacfff98d7eb
halit <halitfuatbatur@gmail.com>
2021-04-14 01:56:23 +0300

    move drawer to a components skeleton for searchResultsContent

---
+++ src/components/searchResultsDrawer/SerchResultsDrawer.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import InsertDriveFileIcon from "@material-ui/icons/InsertDriveFile";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import {
+  Divider,
+  ListItemSecondaryAction,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core";
+
+const drawerWidth = 360;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+      zIndex: theme.zIndex.drawer,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerContainer: {
+      overflow: "auto",
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(4),
+    },
+  })
+);
+
+interface SearchResultsDrawerProps {
+  resultStats: {
+    repos: number;
+    bookmarks: number;
+    users: number;
+  };
+}
+
+const SerchResultsDrawer: React.FC<SearchResultsDrawerProps> = ({
+  resultStats,
+}) => {
+  const { repos, users, bookmarks } = resultStats;
+  const classes = useStyles();
+
+  return (
+    <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <Toolbar />
+      <div className={classes.drawerContainer}>
+        <List>
+          {[
+            {
+              text: "Repositories",
+              icon: <InsertDriveFileIcon />,
+              count: repos,
+            },
+            { text: "Users", icon: <InsertEmoticonIcon />, count: users },
+            {
+              text: "Bookmark",
+              icon: <BookmarkBorderIcon />,
+              count: bookmarks,
+            },
+          ].map((item, index) => (
+            <ListItem button key={item.text}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+              <ListItemSecondaryAction>{item.count}</ListItemSecondaryAction>
+              <ListItem />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </div>
+    </Drawer>
+  );
+};
+
+export default SerchResultsDrawer;
--- src/container/searchResults/SearchResults.tsx
+++ src/container/searchResults/SearchResults.tsx
@@ -1,38 +1,14 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import InsertDriveFileIcon from "@material-ui/icons/InsertDriveFile";
-import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
-import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
-import {
-  Divider,
-  ListItemSecondaryAction,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-} from "@material-ui/core";
-
-const drawerWidth = 360;
+import SearchResultsDrawer from "../../components/searchResultsDrawer/SerchResultsDrawer";
+import SearchResultsContent from "../../components/searchResultsContent/SearchResultsContent";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       display: "flex",
-    },
-    drawer: {
-      width: drawerWidth,
-      flexShrink: 0,
-      zIndex: theme.zIndex.drawer,
-    },
-    drawerPaper: {
-      width: drawerWidth,
-    },
-    drawerContainer: {
-      overflow: "auto",
     },
     content: {
       flexGrow: 1,
@@ -80,43 +56,12 @@ const SearchResults = () => {
   }, [query.get("query")]);
 
   return (
-    <div className={classes.root}>
+    <div style={{ display: "flex" }}>
       <CssBaseline />
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <Toolbar />
-        <div className={classes.drawerContainer}>
-          <List>
-            {[
-              { text: "Repositories", count: 59 },
-              { text: "Users", count: 59 },
-              { text: "Bookmar", count: 59 },
-            ].map((item, index) => (
-              <ListItem button key={item.text}>
-                <ListItemIcon>
-                  {index === 0 ? (
-                    <InsertDriveFileIcon />
-                  ) : index === 1 ? (
-                    <InsertEmoticonIcon />
-                  ) : (
-                    <BookmarkBorderIcon />
-                  )}
-                </ListItemIcon>
-                <ListItemText primary={item.text} />
-                <ListItemSecondaryAction>{item.count}</ListItemSecondaryAction>
-                <ListItem />
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-        </div>
-      </Drawer>
-      <main className={classes.content}>Hello</main>
+      <SearchResultsDrawer resultStats={{ repos: 5, bookmarks: 5, users: 5 }} />
+      <main className={classes.content}>
+        <SearchResultsContent />
+      </main>
     </div>
   );
 };
