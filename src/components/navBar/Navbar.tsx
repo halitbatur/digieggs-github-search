3119318cf8c99910233a51723c829f0427f86368
halit <halitfuatbatur@gmail.com>
2021-04-13 17:11:08 +0300

    add bookmark part and install react-router

--- src/components/navBar/Navbar.tsx
+++ src/components/navBar/Navbar.tsx
@@ -5,85 +5,76 @@ import {
   Theme,
   createStyles,
 } from "@material-ui/core/styles";
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  InputBase,
-} from "@material-ui/core";
+import { AppBar, Toolbar, Typography, InputBase } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
+import { Link, useLocation } from "react-router-dom";
 import BookmarkBorderOutlinedIcon from "@material-ui/icons/BookmarkBorderOutlined";
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    grow: {
-      flexGrow: 1,
-    },
-
-    title: {
-      display: "none",
-      [theme.breakpoints.up("sm")]: {
-        display: "block",
-      },
-    },
-    search: {
-      position: "relative",
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.black, 0.25),
-      "&:hover": {
-        backgroundColor: fade(theme.palette.common.black, 0.5),
-      },
-      marginRight: theme.spacing(2),
-      marginLeft: 0,
-      width: "100%",
-      [theme.breakpoints.up("sm")]: {
-        marginLeft: theme.spacing(3),
-        width: "auto",
-      },
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: "100%",
-      position: "absolute",
-      pointerEvents: "none",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      "&:hover": {
-        cursor: "pointer",
-      },
-      zIndex: 2,
-    },
-    inputRoot: {
-      color: "inherit",
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("md")]: {
-        width: "20ch",
-      },
-    },
-    sectionDesktop: {
-      display: "none",
-      [theme.breakpoints.up("md")]: {
-        display: "flex",
-      },
-    },
-    sectionMobile: {
-      display: "flex",
-      [theme.breakpoints.up("md")]: {
-        display: "none",
-      },
-    },
-  })
-);
-
 export default function PrimarySearchAppBar() {
+  const location = useLocation();
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      grow: {
+        flexGrow: 1,
+      },
+
+      title: {
+        display: "none",
+        [theme.breakpoints.up("sm")]: {
+          display: "block",
+        },
+      },
+      search: {
+        position: "relative",
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.black, 0.25),
+        "&:hover": {
+          backgroundColor: fade(theme.palette.common.black, 0.5),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: "100%",
+        [theme.breakpoints.up("sm")]: {
+          marginLeft: theme.spacing(3),
+          width: "auto",
+        },
+      },
+      searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: "100%",
+        position: "absolute",
+        pointerEvents: "none",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        "&:hover": {
+          cursor: "pointer",
+        },
+        zIndex: 2,
+      },
+      inputRoot: {
+        color: "inherit",
+      },
+      inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("md")]: {
+          width: "20ch",
+        },
+      },
+      bookMark: {
+        padding: theme.spacing(1, 1, 1, 0),
+        display: "flex",
+        backgroundColor:
+          location.pathname === "/bookmark"
+            ? fade(theme.palette.common.white, 0.25)
+            : "inherit",
+      },
+    })
+  );
   const classes = useStyles();
 
   return (
@@ -107,10 +98,12 @@ export default function PrimarySearchAppBar() {
               inputProps={{ "aria-label": "search" }}
             />
           </div>
-          <BookmarkBorderOutlinedIcon />
-          <Typography className={classes.title} variant="subtitle1" noWrap>
-            Bookmarks
-          </Typography>
+          <div className={classes.bookMark}>
+            <BookmarkBorderOutlinedIcon />
+            <Typography className={classes.title} variant="subtitle1" noWrap>
+              Bookmarks
+            </Typography>
+          </div>
           <div className={classes.grow} />
         </Toolbar>
       </AppBar>
