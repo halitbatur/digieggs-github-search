fc3d8c41a72192c5bbf368906022c6cbdb3aa5d9
halit <halitfuatbatur@gmail.com>
2021-04-16 06:28:00 +0300

    add drawers to bookmarks

--- src/container/bookmarks/Bookmarks.tsx
+++ src/container/bookmarks/Bookmarks.tsx
@@ -1,20 +1,63 @@
 import React from "react";
 import SearchResultsContent from "../../components/searchResultsContent/SearchResultsContent";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import Toolbar from "@material-ui/core/Toolbar";
 import { connect } from "react-redux";
-import {
-  addNewBookmark,
-  removeBookMark,
-  repoData,
-} from "../../reducers/action";
+import { repoData } from "../../reducers/action";
+import Drawer from "@material-ui/core/Drawer";
+import { Typography } from "@material-ui/core";
+
+const drawerWidth = 360;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+      zIndex: theme.zIndex.drawer,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerContainer: {
+      overflow: "auto",
+    },
+  })
+);
 
 interface BookmarksProps {
   bookmarks: any;
 }
 
 const Bookmarks: React.FC<BookmarksProps> = ({ bookmarks }) => {
+  const classes = useStyles();
+
   return (
     <div style={{ display: "flex" }}>
-      <SearchResultsContent type="Bookmarks" userPageRepos={bookmarks} />
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <div style={{ padding: "42px 0 0 46px" }}>
+            <BookmarkBorderIcon style={{ width: "64px", height: "64px" }} />
+            <Typography variant="h5" style={{ fontWeight: "bold" }}>
+              Bookmarks
+            </Typography>
+            <Typography variant="subtitle1">
+              You have {bookmarks.length} bookmarks
+            </Typography>
+          </div>
+        </div>
+      </Drawer>
+      <div style={{ padding: "42px 0 0 46px", width: "100%" }}>
+        <SearchResultsContent type="Bookmarks" userPageRepos={bookmarks} />
+      </div>
     </div>
   );
 };
