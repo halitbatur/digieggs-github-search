e304633244abb43addbf8e5a37d85656eafff440
halit <halitfuatbatur@gmail.com>
2021-04-16 05:14:56 +0300

    initial setup for redux

--- src/reducers/action.tsx
+++ src/reducers/action.tsx
@@ -1,12 +1,18 @@
-const bookmark = (state = [], action: Record<string, string>) => {
-  switch (action.type) {
-    case "REMOVE_BOOK_MARK":
-      return [...state, action.payload];
-    case "ADD_NEW_BOOK_MARK":
-      return [...state, , action.payload];
-    default:
-      return state;
-  }
-};
+export interface repoData {
+  name: string;
+  description: string;
+  id: number;
+}
+export function addNewBookmark(payload: Record<string, repoData>) {
+  return {
+    type: "ADD_NEW_BOOK_MARK",
+    payload,
+  };
+}
 
-export default bookmark;
+export function removeBookMark(payload: Record<string, repoData>) {
+  return {
+    type: "REMOVE_BOOK_MARK",
+    payload,
+  };
+}
